2fe85d6b5431a1aa9a3e2f8ba1561f412f538dce
Yuuki Wesp <ls-micro@ya.ru>
2018-08-24 01:16:09 +0300

    create project

---
+++ src/cpu/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace cpu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ src/cpu/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("cpu-host")]
+[assembly: AssemblyProduct("cpu")]
+[assembly: AssemblyCopyright("Copyright © Yuuki Wesp 2018")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d0c24d6b-9ff7-417e-9eab-2c264b23ea79")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
