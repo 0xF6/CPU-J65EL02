be6f8d4af3adece0f6e52fa47be4a8668e4769d9
Yuuki Wesp <ls-micro@ya.ru>
2018-08-28 16:56:35 +0300

    remove js and maps

--- src/screen/app/front.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const xterm_1 = require("xterm");
-const xterm_addon_ligatures_1 = require("xterm-addon-ligatures");
-var xterm = new xterm_1.Terminal();
-xterm.open(document.getElementById('terminal'));
-xterm_addon_ligatures_1.enableLigatures(xterm);
-xterm.writeln("-> {.} >>= etc");
-//# sourceMappingURL=front.js.map
