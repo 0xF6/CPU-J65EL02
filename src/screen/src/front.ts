0dd986e1863e7adb3aef7f8d78f4ff32be85fdc0
Yuuki Wesp <ls-micro@ya.ru>
2018-08-28 16:56:58 +0300

    complete terminal

--- src/screen/src/front.ts
+++ src/screen/src/front.ts
@@ -1,7 +1,23 @@
 import { Terminal } from 'xterm';
+
+import * as fit from 'xterm/lib/addons/fit/fit';
+import * as webLinks from 'xterm/lib/addons/webLinks/webLinks';
+import * as winptyCompat from 'xterm/lib/addons/winptyCompat/winptyCompat';
+
 import {enableLigatures} from 'xterm-addon-ligatures';
 
+Terminal.applyAddon(fit);
+Terminal.applyAddon(webLinks);
+Terminal.applyAddon(winptyCompat);
+const {ipcRenderer} = require('electron')
 var xterm = new Terminal()
+ipcRenderer.on("char", (event, arg) => {
+    xterm.write(arg.char);
+})
 xterm.open(document.getElementById('terminal'));
+xterm.resize(80, 65);
+(xterm as any).webLinksInit();
+(xterm as any).winptyCompatInit();
+xterm.setOption("termName", "vts100");
 enableLigatures(xterm);
-xterm.writeln("-> {.} >>= etc");
+xterm.writeln("Screen J65EL02 - 2018 (C) Yuuki Wesp -->");
--- src/screen/src/index.ts
+++ src/screen/src/index.ts
@@ -1,21 +1,26 @@
-import { app, BrowserWindow, ipcMain, ipcRenderer } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 let win: BrowserWindow = null;
 var express = require('express');
+import * as bodyParser from "body-parser";
 var appw = express();
-
-appw.listen(1337, function(){
-    console.log('Express server listening on port 1337');
+var jsonParser = bodyParser.json()
+appw.post('/p',jsonParser, function (req, res) {
+    if (!req.body) return res.sendStatus(404);
+    if (!req.body.char) return res.sendStatus(400);
+    win.webContents.send("char", {char: req.body.char});
+    res.send('true');
 });
+appw.listen(8666, function(){});
 
 app.on("ready", () => {
     win = new BrowserWindow({
-        width: 1200, height: 860
+        width: 800, height: 583
     });
     win.loadURL(`file://${__dirname}/../assets/index.html`);
     
     win.setMenu(null);
-    win.setResizable(false);
-    win.webContents.openDevTools();
+    win.setResizable(true);
+    //win.webContents.openDevTools();
     win.on("closed", () => {
         win = null;
     });
