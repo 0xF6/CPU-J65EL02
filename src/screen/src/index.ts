4ea8e69edd6de3ceb8bbf29c505c33a563a90a1d
Yuuki Wesp <ls-micro@ya.ru>
2018-08-30 06:21:26 +0300

    added get status and post action char

--- src/screen/src/index.ts
+++ src/screen/src/index.ts
@@ -4,11 +4,14 @@ var express = require('express');
 import * as bodyParser from "body-parser";
 var appw = express();
 var jsonParser = bodyParser.json()
-appw.post('/p',jsonParser, function (req, res) {
-    if (!req.body) return res.sendStatus(404);
+appw.post('/char',jsonParser, function (req, res) {
+    if (!req.body) return res.sendStatus(405);
     if (!req.body.char) return res.sendStatus(400);
     win.webContents.send("char", {char: req.body.char});
-    res.send('true');
+    res.send({status: 0x0});
+});
+appw.get('/status',jsonParser, function (req, res) {
+    res.send({status: 0x0});
 });
 appw.listen(8666, function(){});
 
