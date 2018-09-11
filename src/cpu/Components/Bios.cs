2c541328c151ad300a65d39fca2d75682d107087
Yuuki Wesp <ls-micro@ya.ru>
2018-09-11 19:45:39 +0300

    added bios exceptions

---
+++ src/cpu/Components/Bios.cs
@@ -0,0 +1,41 @@
+﻿namespace vm.components
+{
+    using cpu;
+    using devices;
+
+    public class Bios
+    {
+        private readonly Machine _machine;
+        private readonly int _startPoint;
+        private readonly string _registerInit;
+
+        private RedBus redBus;
+        private CPU cpu;
+        private Memory ram;
+
+        public Bios(Machine machine, int startPoint, string RegisterInit)
+        {
+            _machine = machine;
+            _startPoint = startPoint;
+            _registerInit = RegisterInit;
+        }
+
+        public CPU initCPU()
+        {
+            return cpu = new CPU();
+        }
+
+        public Bus initBus()
+        {
+            redBus = new RedBus(cpu);
+            return new Bus(redBus);
+        }
+
+        public Memory initRAM() => ram = new Memory(read<int>("memory_start_address"), read<int>("memory_end_address"), cpu);
+
+        public void loadBiosFirware(string path)
+        { }
+
+        public T read<T>(string key) => default;
+    }
+}
---
+++ src/cpu/Exceptions/BiosException.cs
@@ -0,0 +1,12 @@
+﻿namespace vm.exceptions
+{
+    using System;
+    public class BiosException : Exception
+    {
+        public string Description { get; set; }
+        public BiosException(string message, string desc) : base(message)
+        {
+            Description = desc;
+        }
+    }
+}
